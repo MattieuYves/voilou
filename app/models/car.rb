1b2287d0b7b17c5f46dfe9eed5f310a29029f23e
Mattieu Yves <mattieuyves@gmail.com>
2024-06-27 14:30:45 +0200

    validations added

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :address, presence: true
+  validates :address, comparison: { greater_than: 0 }
 end
